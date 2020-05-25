96cda16bb96f06783fb6d9e8b5347a67252dd349
isaac5003 <isaac.cortez1294@hotmail.com>
2020-05-25 00:32:06 -0600

    Variable de entorno

--- server/config/config.js
+++ server/config/config.js
@@ -20,7 +20,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://root:vNbJo2lmKxmcjLc2@cluster0-vgd4q.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
